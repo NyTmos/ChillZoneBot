5271344818b4e5f063b6e7d2248cebf62018e0ed
NyTmo <filip.pachner@gmail.com>
2020-03-24 18:35:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,18 +8,6 @@ client.on("ready", () => {
 });
 
 
-bot.on('ready', () => {
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'with depression',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/monstercat"
-        }
-    });
-});
-
-
 
 
 
