3167b3f7a98f2442db953966fe97f6ef162e5ffc
NyTmo <filip.pachner@gmail.com>
2020-03-24 19:49:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,24 @@ client.on("ready", () => {
 });
 
 
+client.on('message', message => {
+	if (message.content === '!play') {
+		if (message.channel.type !== 'text') return;
 
+		const voiceChannel = message.member.voice.channel;
+
+		if (!voiceChannel) {
+			return message.reply('please join a voice channel first!');
+		}
+
+		voiceChannel.join().then(connection => {
+			const stream = ytdl('https://www.youtube.com/watch?v=D57Y1PruTlw', { filter: 'audioonly' });
+			const dispatcher = connection.play(stream);
+
+			dispatcher.on('end', () => voiceChannel.leave());
+		});
+	}
+});
 
 
 client.on('message', message => {
