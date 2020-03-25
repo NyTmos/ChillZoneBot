e78f53c360e30724fc75d9611cdc69bd2407f798
NyTmo <filip.pachner@gmail.com>
2020-03-25 12:51:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,89 +2,49 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
+client.commands = new Collection();
+client.aliases = new Collection();
 
+client.categories = fs.readdirSync("./commands/");
+
+config({
+    path: __dirname + "/.env"
+});
+
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
 
 client.on("ready", () => {
     console.log(`Hi, ${client.user.username} is now online!`);
 
-    // Set the user presence
     client.user.setPresence({
         status: "online",
         game: {
             name: "me getting developed",
-            type: "WATCHING"
+            type: "STREAMING"
         }
     }); 
-})
-
-// When a message comes in, what's in these brackets will be executed
-client.on("message", async message => {
-    console.log(`${message.author.username} said: ${message.content}`);
 });
-
 
 client.on("message", async message => {
     const prefix = "_";
 
-    // If the author's a bot, return
-    // If the message was not sent in a server, return
-    // If the message doesn't start with the prefix, return
     if (message.author.bot) return;
     if (!message.guild) return;
     if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    // Arguments and command variable
-    // cmd is the first word in the message, aka the command
-    // args is an array of words after the command
-    // !say hello I am a bot
-    // cmd == say (because the prefix is sliced off)
-    // args == ["hello", "I", "am", "a", "bot"]
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-    if (cmd === "ping") {
-        // Send a message
-        const msg = await message.channel.send(`🏓 Pinging....`);
-
-        // Edit the message
-        msg.edit(`🏓 Pong!\nLatency is ${Math.floor(msg.createdTimestap - message.createdTimestap)}ms\nAPI Latency is ${Math.round(client.ping)}ms`);
-    }
-
-    if (cmd === "say") {
-        // Check if you can delete the message
-        if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
-        
-        // Role color
-        const roleColor = message.guild.me.highestRole.hexColor;
-
-        // If the first argument is embed, send an embed,
-        // otherwise, send a normal message
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
-                .setTimestamp()
-                .setImage(client.user.displayAvatarURL)
-                .setAuthor(message.author.username, message.author.displayAvatarURL);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
-        }
-    }
+    if (command) 
+        command.run(client, message, args);
 });
-
-// Create an event listener for new guild members
-client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
-  const channel = member.guild.channels.cache.find(ch => ch.name === 'global-chat');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
-});
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
