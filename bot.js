35c7490def5055de9deb2ec52123ea945d44ae91
NyTmo <filip.pachner@gmail.com>
2020-03-25 14:22:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.on("ready", () => {
     console.log(`Hi, ${client.user.username} is now online!`);
 
     client.user.setPresence({
-        status: "dnd",
+        status: "online",
         game: {
             name: "me getting developed",
             type: "WATCHING"
@@ -53,37 +53,6 @@ client.on("message", async message => {
     
     
     /////////////////////////////////////////////////////////////////////////////////    
-    
-
-    if (cmd === "say") {
-        // Check if you can delete the message
-        if (message.deletable) message.delete();
-
-        if (args.length < 0) return message.reply(`Nothing to say?`).then(m => m.delete(5000));
-        
-        // Role color
-        const roleColor = message.guild.me.highestRole.hexColor;
-
-        // If the first argument is embed, send an embed,
-        // otherwise, send a normal message
-        if (args[0].toLowerCase() === "embed") {
-            const embed = new RichEmbed()
-                .setDescription(args.slice(1).join(" "))
-                .setColor(roleColor === "#000000" ? "#ffffff" :  roleColorv)
-                .setTimestamp()
-                .setImage(client.user.displayAvatarURL)
-                .setAuthor(message.author.username, message.author.displayAvatarURL);
-
-            message.channel.send(embed);
-        } else {
-            message.channel.send(args.join(" "));
-        }
-    }
-});
-
-
-  /////////////////////////////////////////////////////////////////////////////////  
-
 
 
 // THIS  MUST  BE  THIS  WAY
