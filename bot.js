9ca8e1a9695474443b60d001ecae63db7e44957e
NyTmo <filip.pachner@gmail.com>
2020-03-25 14:28:00 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,9 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
+// When the bot's online, what's in these brackets will be executed
 client.on("ready", () => {
     console.log(`Hi, ${client.user.username} is now online!`);
 
+    // Set the user presence
     client.user.setPresence({
         status: "online",
         game: {
@@ -14,37 +16,10 @@ client.on("ready", () => {
     }); 
 })
 
+// When a message comes in, what's in these brackets will be executed
 client.on("message", async message => {
-    const prefix = ".";
-
-    // If the author's a bot, return
-    // If the message was not sent in a server, return
-    // If the message doesn't start with the prefix, return
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-
-    // Arguments and command variable
-    // cmd is the first word in the message, aka the command
-    // args is an array of words after the command
-    // !say hello I am a bot
-    // cmd == say (because the prefix is sliced off)
-    // args == ["hello", "I", "am", "a", "bot"]
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();   
-  /////////////////////////////////////////////////////////////////////////////////  
-    
-     if (cmd === "dev") {
-        // Send a message
-        const msg = await message.channel.send(`💿 Loading....`);
-
-        // Edit the message
-        msg.edit(`🛠 **Comming Soon** 🛠! \nA-Team`);
-    }   
-    
-    
-    /////////////////////////////////////////////////////////////////////////////////    
-
+    console.log(`${message.author.username} said: ${message.content}`);
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
