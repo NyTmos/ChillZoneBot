4069b3733f2db058be78e9ae39b80041083da368
NyTmo <filip.pachner@gmail.com>
2020-03-25 12:21:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ client.on("ready", () => {
     console.log(`Hi, ${client.user.username} is now online!`); 
 });
 
+client.user.setActivity(`NEJAKE TY DEPRESE JA NEVIM CO S TIM MAS`, { type: 'PLAYING' }); 
 
 client.on('message', message => {
 	console.log(message.content);
