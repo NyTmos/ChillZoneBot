c1b45aed677c3b58e79f41b64e5f8667348c7690
NyTmo <filip.pachner@gmail.com>
2020-03-25 14:14:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,5 +66,17 @@ client.on("message", async message => {
         }
     }
 });
+
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'global-chat);
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
